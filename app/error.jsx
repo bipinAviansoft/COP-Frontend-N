5781dbd993727f786f92a930250011ba8650d633
bipinAviansoft <bipinkumar@aviansoft.in>
2025-04-26 13:53:58 +0530

    fuel & tolls done but map direction google api working

--- app/error.jsx
+++ app/error.jsx
@@ -1,12 +1,15 @@
 "use client"; // Error boundaries must be Client Components
 
 import Button from "@/components/ui/button";
+// app/not-found.js or app/not-found.tsx
+import { redirect } from "next/navigation";
 
 export default function Error({ error, reset }) {
+  // redirect("/"); // Redirect to homepage
   return (
     <div className="min-h-80 flex flex-col justify-center items-center bg-rose-100">
       <h2 className="text-3xl font-semibold text-rose-500">
-        An error ocurred! 🥺 
+        An error ocurred! 🥺
       </h2>
       <code className="my-4 px-2 py-1 rounded-md bg-white/30">
         {error.message ||
